246d96fdf0df5ae77bfbacea40f4147b0fcb19c0
Ryan Fuller <fullerra@hendrix.edu>
2023-11-01 17:11:43 -0500

    Create Student Database

---
+++ WalterUniversity/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+using WalterUniversity.Data;
+
+namespace WalterUniversity.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly WalterUniversity.Data.SchoolContext _context;
+
+        public DetailsModel(WalterUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+      public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Student = student;
+            }
+            return Page();
+        }
+    }
+}
---
+++ WalterUniversity/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+using WalterUniversity.Data;
+
+namespace WalterUniversity.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WalterUniversity.Data.SchoolContext _context;
+
+        public IndexModel(WalterUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Students != null)
+            {
+                Student = await _context.Students.ToListAsync();
+            }
+        }
+    }
+}
