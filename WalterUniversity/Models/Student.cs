0ede3345daffee147fd78a2e288c38822d2ec784
Ryan Fuller <fullerra@hendrix.edu>
2023-11-01 19:33:29 -0500

    Finished Step 2

--- WalterUniversity/Models/Student.cs
+++ WalterUniversity/Models/Student.cs
@@ -6,6 +6,7 @@ namespace ContosoUniversity.Models
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
+        // public int Age { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
