1bc216295dd6387ed746bda0ae8dc9f563c1862c
Ryan Fuller <fullerra@hendrix.edu>
2023-11-01 21:14:21 -0500

    Created Instructors Page

---
+++ WalterUniversity/Pages/Instructors/Index.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+using WalterUniversity.Data;
+
+namespace WalterUniversity.Pages.Instructors
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WalterUniversity.Data.SchoolContext _context;
+
+        public IndexModel(WalterUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Instructor> Instructor { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Instructors != null)
+            {
+                Instructor = await _context.Instructors.ToListAsync();
+            }
+        }
+    }
+}
