541d1e43fdf9efa513eee94271f3ea643b9f915b
Ryan Fuller <fullerra@hendrix.edu>
2023-11-01 21:02:19 -0500

    Finished step 5, added I and W to Grade enum

--- WalterUniversity/Migrations/20231102015926_InitialCreate.Designer.cs
+++ WalterUniversity/Migrations/20231102015926_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WalterUniversity.Data;
 
@@ -10,9 +11,11 @@ using WalterUniversity.Data;
 namespace WalterUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20231102015926_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.13");
